9fc9249d432d5f22f39619635963f6ba14d457f9
Nikolaos Papadopoulos <nikolaspapadop@yahoo.com>
2013-07-04 16:57:57 +0200

    provide directories for build testing

---
+++ packages/norsexml-0.1/.pc/found-bin-fix/setup.py
@@ -0,0 +1,10 @@
+#/usr/bin/python
+
+from setuptools import setup
+setup(	name='norsexml',
+		version='0.1',
+		description='Bla bla bla',
+		author='Nikolaos Papadopoulos',
+		author_email='papadopn@in.tum.de',
+		py_modules=['norsexml'],
+		requires=['argparse', 'lxml'])
---
+++ packages/norsexml-0.1/debian/norsexml/usr/lib/python2.7/dist-packages/norsexml.py
@@ -0,0 +1,1 @@
+../../../share/pyshared/norsexml.py
